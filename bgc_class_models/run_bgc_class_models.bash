fdc011db41ca97f2b88693dc2c7d4c6e69aadf11
pereiramemo <pereiramemo@gmail.com>
2017-08-17 17:32:37 +0200

    added bgc_class_models

---
+++ bgc_class_models/run_bgc_class_models.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -o errexit
+# set -o nounset
+
+function realpath() {
+    echo $(readlink -f $1 2>/dev/null )
+}
+
+# handle input file
+INPUT_FILE=$(basename $1)
+INPUT_DIR=$(dirname $(realpath $1))
+shift
+
+# handle output file
+OUTPUT_FILE=$(basename $1)
+touch "${OUTPUT_FILE}"
+OUTPUT_DIR=$(dirname $(realpath "${OUTPUT_FILE}" ))
+shift
+
+# Links within the container
+CONTAINER_SRC_DIR=/input
+CONTAINER_DST_DIR=/output
+
+
+docker run \
+  --volume ${INPUT_DIR}:${CONTAINER_SRC_DIR}:rw \
+  --volume ${OUTPUT_DIR}:${CONTAINER_DST_DIR}:rw \
+  --detach=false \
+  --rm \
+   epereira/ufbgctoolbox:bgc_class_models \
+  --input "${CONTAINER_SRC_DIR}/${INPUT_FILE}" \
+  --output "${OUTPUT_FILE}" \
+  $@
+
+
