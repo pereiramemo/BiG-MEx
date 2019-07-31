66376bee64c5f766f050ffb4d69f9a6d5f019370
pereiramemo <pereiramemo@gmail.com>
2019-07-31 03:53:17 +0200

    added confs

---
+++ code/copy_command.bash
@@ -0,0 +1,5 @@
+cp ../bgc_dom_annot/resources/bgc_dom_annot.bash bgc_dom_annot/
+cp ../bgc_dom_amp_div/resources/bgc_dom_amp_div.bash bgc_dom_amp_div/
+cp ../bgc_dom_meta_div/resources/bgc_dom_meta_div.bash bgc_dom_meta_div/
+cp ../bgc_dom_merge_div/resources/bgc_dom_merge_div.bash bgc_dom_merge_div/
+cp ../bgc_class_pred/resources/bgc_class_pred.bash bgc_class_pred/
